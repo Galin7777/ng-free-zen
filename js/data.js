84463ffe8b9de80c9199593a7dbefd2f9cd0c9ff
Galin7777 <kazakova0309@yandex.ru>
2024-07-25 12:26:17 +0300

    WIP

--- js/data.js
+++ js/data.js
@@ -122,34 +122,37 @@ export const clients = {
 };
 
 export const footer = {
-  columns: {
-    title: 'Title learn',
-      links: [
-      {
-        url: 'https://example.com',
-        name:'Help center',
-      },
-      {
-        url: 'https://example.com',
-        name:'Join ZEN team',
-      },
-      {
-        url: 'https://example.com',
-        name:'Privacy Policy',
-      },
-      {
-        url: 'https://example.com',
-        name:'Cookies',
-      },
-      {
-        url: 'https://example.com',
-        name:'Terms of use',
-      },
-    ],
-    title: 'Title discover',
+  columns: [
+    {
+      title: 'Title learn',
       links: [
         {
           url: 'https://example.com',
+          name:'Help center',
+        },
+        {
+          url: 'https://example.com',
+          name:'Join ZEN team',
+        },
+        {
+          url: 'https://example.com',
+          name:'Privacy Policy',
+        },
+        {
+          url: 'https://example.com',
+          name:'Cookies',
+        },
+        {
+          url: 'https://example.com',
+          name:'Terms of use',
+        },
+      ],
+    },
+    {
+      title: 'Title discover',
+      links: [
+        {
+         url: 'https://example.com',
           name:'For developers',
         },
         {
@@ -161,45 +164,47 @@ export const footer = {
           name:'Google Pay',
         },
       ],
-    title: 'Title offer',
-      links:[
-        {
-          url: 'https://example.com',
-          name:'Business',
-        },
-        {
-          url: 'https://example.com',
-          name:'Personal',
-        },
-      ],
-    title: 'Title language',
-      links: [
-        {
-          url: 'https://example.com',
-          name:'English',
-        },
-        {
-          url: 'https://example.com',
-          name:'Polish',
-        },
-      ],
-    title: 'Title follow us',
-      links: [
-        {
-          url: 'https://example.com',
-        },
-      ],
-      image: {
-          source: './assets/icons/in.svg',
-          description: 'Linkedin',
-        },
-    title: 'Title contact',
-      links: [
-        {
-          url: 'mailto:hello@zen.com',
-        },
-      ],
-     },
+    }, 
+  //     title: 'Title offer',
+  //     links:[
+  //       {
+  //         url: 'https://example.com',
+  //         name:'Business',
+  //       },
+  //       {
+  //         url: 'https://example.com',
+  //         name:'Personal',
+  //       },
+  //     ],
+  //     title: 'Title language',
+  //     links: [
+  //       {
+  //         url: 'https://example.com',
+  //         name:'English',
+  //       },
+  //       {
+  //         url: 'https://example.com',
+  //         name:'Polish',
+  //       },
+  //       ],
+  //     title: 'Title follow us',
+  //     links: [
+  //       {
+  //         url: 'https://example.com',
+  //       },
+  //       ],
+  //     image: {
+  //           source: './assets/icons/in.svg',
+  //           description: 'Linkedin',
+  //         },
+  //     title: 'Title contact',
+  //     links: [
+  //       {
+  //         url: 'mailto:hello@zen.com',
+  //       },
+  //     ],
+  //   },
+  ],
   info: {
     texts: ['Paragraph 1', 'Paragraph 2', 'Paragraph 3', 'Paragraph 4' ,'Paragraph 5', 'Paragraph 6'],
     developer: {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,95 @@
 import {header, download, warranty, care, cashback, clients, footer} from './data.js';
 
-console.log(footer.info.texts[1]);
+// console.log(footer.info.texts[1]);
+console.log(footer.columns[0].links[0].url);
+
+
+
+// export const footer = {
+//   columns: {
+//     title: 'Title learn',
+//       links: [
+//       {
+//         url: 'https://example.com',
+//         name:'Help center',
+//       },
+//       {
+//         url: 'https://example.com',
+//         name:'Join ZEN team',
+//       },
+//       {
+//         url: 'https://example.com',
+//         name:'Privacy Policy',
+//       },
+//       {
+//         url: 'https://example.com',
+//         name:'Cookies',
+//       },
+//       {
+//         url: 'https://example.com',
+//         name:'Terms of use',
+//       },
+//     ],
+//     title: 'Title discover',
+//       links: [
+//         {
+//           url: 'https://example.com',
+//           name:'For developers',
+//         },
+//         {
+//           url: 'https://example.com',
+//           name:'Apple pay',
+//         },
+//         {
+//           url: 'https://example.com',
+//           name:'Google Pay',
+//         },
+//       ],
+//     title: 'Title offer',
+//       links:[
+//         {
+//           url: 'https://example.com',
+//           name:'Business',
+//         },
+//         {
+//           url: 'https://example.com',
+//           name:'Personal',
+//         },
+//       ],
+//     title: 'Title language',
+//       links: [
+//         {
+//           url: 'https://example.com',
+//           name:'English',
+//         },
+//         {
+//           url: 'https://example.com',
+//           name:'Polish',
+//         },
+//       ],
+//     title: 'Title follow us',
+//       links: [
+//         {
+//           url: 'https://example.com',
+//         },
+//       ],
+//       image: {
+//           source: './assets/icons/in.svg',
+//           description: 'Linkedin',
+//         },
+//     title: 'Title contact',
+//       links: [
+//         {
+//           url: 'mailto:hello@zen.com',
+//         },
+//       ],
+//      },
+//   info: {
+//     texts: ['Paragraph 1', 'Paragraph 2', 'Paragraph 3', 'Paragraph 4' ,'Paragraph 5', 'Paragraph 6'],
+//     developer: {
+//       url:'https://example.com',
+//       source: '../assets/icons/developer.svg',
+//       description: 'Pci',
+//     },
+//   },
+// };
