dc9ae1430a21f73274d5b087cc6e79b0ce83d95a
Galin7777 <kazakova0309@yandex.ru>
2024-08-26 16:49:01 +0300

    Added Warranty component

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 import { data } from './data.js';
 import { Header } from './widjets/Header/Header.js';
 import { Download } from './widjets/Download/Download.js';
+import { Warranty } from './widjets/Warranty/Warranty.js';
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 
 const $logo = document.querySelector('#logo');
 
