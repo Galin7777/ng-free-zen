029b9c6369fc8a2d1d3ad003ab9971799f39ec12
Galin7777 <kazakova0309@yandex.ru>
2024-08-31 17:10:48 +0300

    Added Clients component

---
+++ js/widjets/Clients/types.ts
@@ -0,0 +1,14 @@
+export type LogoData = {
+  lightSource: string;
+  darkSource: string;
+};
+
+export type BrandData = {
+  name: string;
+  logo: LogoData;
+};
+
+
+export type ClientsData = {
+  brands: BrandData[];
+};
