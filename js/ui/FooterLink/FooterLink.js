61f58a35c2cb9ac8eadac3fd8c8ed0ad025e91d2
Galin7777 <kazakova0309@yandex.ru>
2024-09-07 20:54:14 +0300

    Added FooterLink component

---
+++ js/ui/FooterLink/FooterLink.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {import ('../../widjets/Footer/types').LinkData} LinkData
+ */
+
+/**
+ * @function FooterLink
+ * @param {LinkData} linkData
+ * @returns {string} HTML
+ */
+
+export const FooterLink = (linkData) => `
+  <a href="${linkData.url}">
+    ${linkData.content.type === 'text'
+    ? linkData.content.data
+    : `<img src="${linkData.content.data}" alt= "Linkedin"/>`}
+  </a>
+`;
