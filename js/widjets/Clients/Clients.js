772dd01c963990c1ebd0b44701d5b6cbff5a0207
Galin7777 <kazakova0309@yandex.ru>
2024-09-15 17:49:02 +0300

    Refactoring file

--- js/widjets/Clients/Clients.js
+++ js/widjets/Clients/Clients.js
@@ -13,10 +13,10 @@ export const Clients = (data) => {
   return `
     <section class="clients" id="clients">
       <div class="clients__wrapper">
-        <ul class="clients__brands">
+        <ul class="clients__brands" id="brands">
           ${brands.map((brand) => `
             <li class="clients__brand">
-              <img src="${brand.logo.lightSource}" alt="${brand.name}">
+              <img src="${brand.logo.lightSource}" alt="${brand.name}" data-id = "brand">
             </li>
           `).join('')}
         </ul>
