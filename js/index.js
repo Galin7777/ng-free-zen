35a3d94eb0979a769e4d46569067624e29b7f29b
Galin7777 <kazakova0309@yandex.ru>
2024-09-15 17:08:09 +0300

    Added handler for theme

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { data } from './data.js';
+import { dataFromAPI } from './data.js';
 import { Header } from './widjets/Header/Header.js';
 import { Download } from './widjets/Download/Download.js';
 import { Warranty } from './widjets/Warranty/Warranty.js';
@@ -6,18 +6,20 @@ import { Care } from './widjets/Care/Care.js';
 import { Cashback } from './widjets/Cashback/Cashback.js';
 import { Clients } from './widjets/Clients/Clients.js';
 import { Footer } from './widjets/Footer/index.js';
-import { handleLogoClick } from './handlers.js';
+import { handleLogoClick, onThemeClick } from './handlers.js';
 
 const $root = document.querySelector('#root');
 
-$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
-$root?.insertAdjacentHTML('beforeend', Download(data.download));
-$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
-$root?.insertAdjacentHTML('beforeend', Care(data.care));
-$root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
-$root?.insertAdjacentHTML('beforeend', Clients(data.clients));
-$root?.insertAdjacentHTML('beforeend', Footer(data.secondaryInfo));
+$root?.insertAdjacentHTML('beforeend', Header(dataFromAPI.primaryInfo));
+$root?.insertAdjacentHTML('beforeend', Download(dataFromAPI.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(dataFromAPI.warranty));
+$root?.insertAdjacentHTML('beforeend', Care(dataFromAPI.care));
+$root?.insertAdjacentHTML('beforeend', Cashback(dataFromAPI.cashback));
+$root?.insertAdjacentHTML('beforeend', Clients(dataFromAPI.clients));
+$root?.insertAdjacentHTML('beforeend', Footer(dataFromAPI.secondaryInfo));
 
 const $logoBtn = document.querySelector('#logo');
+const $themeBtn = document.querySelector('#theme');
 
 $logoBtn?.addEventListener('click', handleLogoClick);
+$themeBtn?.addEventListener('click', (event) => onThemeClick(event, dataFromAPI.clients.brands));
