84fee8910b6761894398d94170c043995428f26b
Galin7777 <kazakova0309@yandex.ru>
2024-09-17 17:50:53 +0300

    Delited

--- js/widjets/Care/types.ts
+++
@@ -1,8 +0,0 @@
-import type { TitleData } from '../../ui/Title/types';
-import type { ImageData } from '../../ui/Image/types';
-
-export type CareData = {
-  title: TitleData;
-  texts: string[];
-  image: ImageData;
-};
--- js/widjets/Header/types.ts
+++
@@ -1,7 +0,0 @@
-import { LangData } from '../../features/Lang/types';
-import { NavItemData } from '../../features/Navigation/types';
-
-export type HeaderData = {
-  langs: LangData[];
-  navigation: NavItemData[];
-};
