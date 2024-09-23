d14586012f449522268f487cb8797539db5ec1f5
Galin7777 <kazakova0309@yandex.ru>
2024-09-23 21:12:25 +0300

    Added handler for Burger and Navigation

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ import { dataFromAPI } from './data.js';
 import { App } from './App.js';
 import { handleLogoClick } from './handlers.js';
 import { onThemeClick,  } from './handlers.js';
-import { onBurgerClick } from './handlers.js';
+import { handleBurgerClick } from './handlers.js';
+import { handleNavClick } from './handlers.js';
 
 const $root = document.querySelector('#root');
 
@@ -11,7 +12,9 @@ $root?.insertAdjacentHTML('beforeend', App(dataFromAPI));
 const $logoBtn = document.querySelector('#logo');
 const $themeBtn = document.querySelector('#theme');
 const $burgerBtn = document.querySelector('#burger');
+const $nav = document.querySelector('#nav');
 
 $logoBtn?.addEventListener('click', handleLogoClick);
 $themeBtn?.addEventListener('click', (event) => onThemeClick(event, dataFromAPI.clients.brands));
-$burgerBtn?.addEventListener('click', onBurgerClick);
+$burgerBtn?.addEventListener('click', handleBurgerClick);
+$nav?.addEventListener('click', handleNavClick);
